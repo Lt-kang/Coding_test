3b9426afbede87b20a32affba1a9ad120776cad5
Xenrose <114465217+Xenrose@users.noreply.github.com>
2023-06-24 07:30:04 +0900

    [개선 코드 추가]_n개 간격의 원소들.py
    주석 추가

--- 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.py
+++ 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.py
@@ -5,3 +5,9 @@ def solution(num_list, n):
         if 0+n*cnt<len(num_list): answer.append(num_list[0+n*cnt]); cnt+=1
         else: break
     return answer
+
+###########################
+# 더 깔끔한 코드
+# Slicing을 잘 활용하자.
+def solution(num_list, n):
+    return num_list[::n]
