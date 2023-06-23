bd25ce603205534c45ce8c2df5243e4c628e22a4
Xenrose <114465217+Xenrose@users.noreply.github.com>
2023-06-24 08:32:52 +0900

    [개선코드] Update 피보나치 수.py

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -4,3 +4,11 @@ def solution(n):
     for i in range(2,n+1):
         fibo[i] = fibo[i-1] + fibo[i-2]
     return fibo[-1]%1234567
+
+# 개선 코드(피보나치 수열의 더 간단한 코드)
+# 바로 해석 가능하므로 주석은 달지 않음.
+def fibonacci(num):
+    a, b = 0, 1
+    for i in range(num):
+        a, b = b, a+b
+    return a
