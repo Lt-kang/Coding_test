6014e2c0de11ef60785ca446e78d60f9c7d0c893
Xenrose <114465217+Xenrose@users.noreply.github.com>
2023-06-24 07:18:35 +0900

    Update 약수의 합.py
    주석 추가

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.py
@@ -1,8 +1,9 @@
 from math import sqrt
 def solution(n):
     answer = []
-    for i in range(1, int(sqrt(n))+1):
+    for i in range(1, int(sqrt(n))+1): # 정수 n의 약수는 1부터 정수 n의 제곱근 중 나눈 나머지가 0인 i와 n을 i로 나눈 값 두가지가 n의 약수가 된다.
         if n%i==0: 
             answer.append(i)
             answer.append(n//i)
-    return sum(list(set(answer)))
+    return sum(list(set(answer))) # 이를테면 정수 n이 4일 경우 약수가 [1,2,4]가 되는데 위 식을 사용하면 [1,2,2,4]가 되므로 중복수를 제거하기 위해 set() 함수를 사용하였다.
+
