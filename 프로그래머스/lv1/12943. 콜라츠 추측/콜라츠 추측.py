c68adc51f8b452035aa71cfd75c80fd2f27609d6
Xenrose <114465217+Xenrose@users.noreply.github.com>
2023-06-24 11:14:14 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,8 @@
+def solution(num):
+    cnt = 0
+    while num != 1:
+        cnt += 1
+        if num%2==0: num //= 2
+        elif num%2!=0: num = num*3 + 1
+        if cnt == 500: cnt = -1; break
+    return cnt
