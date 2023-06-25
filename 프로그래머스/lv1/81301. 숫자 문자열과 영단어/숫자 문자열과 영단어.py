df888a151691167e589d2927535e2ed66af0125f
Xenrose <114465217+Xenrose@users.noreply.github.com>
2023-06-25 10:12:16 +0900

    [개선코드] Update 숫자 문자열과 영단어.py

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -11,3 +11,13 @@ def solution(s):
     .replace("eight","8")\
     .replace("nine","9")
     return int(s)
+
+# 개선코드
+num_dic = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
+
+def solution(s):
+    answer = s
+    for key, value in num_dic.items():
+        answer = answer.replace(key, value)
+    return int(answer)
+# 딕셔너리를 통해서도 해결할 수 있다.
