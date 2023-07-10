7685b6afd304e9783a765add63ef8e7d823dac26
Xenrose <114465217+Xenrose@users.noreply.github.com>
2023-07-11 00:02:19 +0900

    [level 0] Title: 문자열 밀기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.py
@@ -0,0 +1,8 @@
+from collections import deque
+def solution(A, B):
+    if A==B: return 0
+    A = deque(A)
+    for i in range(1,len(A)+1):
+        A.rotate(1)
+        if ''.join(A)==B: return i
+    return -1
