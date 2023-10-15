774b8d8b0f8ebcd879f3bf3234529df8d49c06eb
Xenrose <114465217+Xenrose@users.noreply.github.com>
2023-10-15 22:24:35 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 128 ms, Memory: 32440 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+queue = deque()
+answer = []
+
+n, k = map(int, input().split())
+
+for i in range(1, n+1):
+    queue.append(i)
+
+while queue:
+    for i in range(k-1): #k=3 일때 range(2) 즉, 0, 1 인덱스 제거
+        queue.append(queue.popleft())
+    answer.append(queue.popleft())
+
+print("<",end='')
+for i in range(len(answer)-1):
+    print("%d, "%answer[i], end='')
+print(answer[-1], end='')
+print(">")
